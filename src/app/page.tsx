60f2e58e6a7debf2bc6e1b76caad8e514476a38b
javad-kuttappi <javaadde123@hotmail.com>
2025-11-09 10:37:03 +0530

    created home page without navigation bar in home section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,64 +2,33 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
+    <div className="h-screen w-full relative">
+      <nav className="text-center p-2">
+        <ul className="hidden md:flex items-center px-15 py-2 justify-between font-grotesk font-normal">
+          <li className="text-lg font-semibold cursor-pointer">Home</li>
+          <li className="text-lg font-semibold cursor-pointer">About</li>
+          <li className="text-lg font-semibold cursor-pointer">Projects</li>
+          <li className="text-lg font-semibold cursor-pointer">Contact</li>
+        </ul>
+
+         <span className="text-xl md:hidden font-semibold font-grotesk" >Menue</span>
+
+      </nav>
+
+      <main className="flex flex-col items-center justify-center mt-20">
+        <h1 className="md:text-9xl text-6xl font-silkscreen">JAVAD</h1>
+
+        <p className="font-jetbrains mt-20  w-35 text-center">
+          CRAFTING DIGITAL EXPERIENCES THAT MATTERS
+        </p>
       </main>
+
+      <div className="w-55 p-2 inline absolute bottom-0 left-0 ">
+        <p className="font-jetbrains">CRIATIVE AND DEVELOPMENT TO TECH AND CULTURAL CHANGEMAKING</p>
+      </div>
+
+          <div className="md:w-20 md:h-20 h-18 w-18 cursor-pointer font-jetbrains absolute bottom-5 right-5 rounded-full bg-blue-500 text-center"> <p className="p-4">LETS,<br/>TALK</p> </div>
+
     </div>
   );
 }
