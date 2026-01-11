192e29f288d6c6650b407eef7ecb20cd22c6b5f9
Javad <javaadde123@hotmail.com>
2026-01-12 04:54:13 +0530

    feat(favicons): added favicones for better search experience

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,6 +22,16 @@ export const metadata: Metadata = {
   description: "Explore the portfolio of Mohammed Javad (Javaadde), a MERN stack developer specializing in React, Node.js, and 3D design. View my projects and GitHub contributions.",
   keywords: ["Javaadde", "Mohammed Javad", "MERN Stack Developer", "React Developer", "Node.js Developer", "3D Design", "Portfolio"],
   authors: [{ name: "Mohammed Javad", url: "https://javade.xyz" }],
+  icons: {
+    icon: [
+      { url: "/favicons/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/favicons/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+      { url: "/favicons/favicon.ico" },
+    ],
+    apple: [
+      { url: "/favicons/apple-touch-icon.png" },
+    ],
+  },
   manifest: "/manifest.json",
   alternates: {
     canonical: "https://javade.xyz",
@@ -76,7 +86,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/logo.png" type="image/png" />
+        <link rel="icon" href="/favicons/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" />
+        <link rel="manifest" href="/manifest.json" />
         <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Silkscreen:wght@400;700&family=Stalinist+One&display=swap" rel="stylesheet"></link>
         <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
       </head>
